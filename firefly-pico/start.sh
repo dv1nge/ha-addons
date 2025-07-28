eb9943b2f91d2de3bdd8dfd710930c5d734ec525
dv1nge <130612241+dv1nge@users.noreply.github.com>
2025-07-29 01:22:01 +0200

    Create start.sh

---
+++ firefly-pico/start.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Read values from /data/options.json (set by HA add-on config UI)
+export FIREFLY_URL=$(jq -r .FIREFLY_URL /data/options.json)
+export FIREFLY_TOKEN=$(jq -r .FIREFLY_TOKEN /data/options.json)
+
+# Optionally, print for debug (can comment out in production)
+echo "FIREFLY_URL=$FIREFLY_URL"
+echo "FIREFLY_TOKEN=$FIREFLY_TOKEN"
+
+# Exec the real Firefly Pico start command (from original image)
+exec /entrypoint.sh
